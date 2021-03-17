dab02dc140759b299bff6917af68af57ca61d991
GoodbyeFirefly <1961192090@qq.com>
2021-03-17 10:49:46 +0800

    bugs2:两个相同的if分支

--- src/mian.java
+++ src/mian.java
@@ -1,5 +1,15 @@
 public class mian {
     public static void main(String[] args) {
+        // bug1:除零
         int a = 1 / 0;
+        System.out.println(a);
+
+        // bug2:相同的两个if分支
+        boolean bool = true;
+        if(bool) {
+            System.out.println("1");
+        } else {
+            System.out.println("1");
+        }
     }
 }
