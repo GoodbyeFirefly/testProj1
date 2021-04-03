615ffddcfd6c9ba2ea39ea9ae46d2db78e6c4f1d
GoodbyeFirefly <1961192090@qq.com>
2021-04-03 16:54:24 +0800

    新增缺陷、修复缺陷、删除缺陷，用来测试git diff中代码变动处插入issues结果。

--- src/mian.java
+++ src/mian.java
@@ -5,8 +5,8 @@ import java.util.Random;
 
 public class mian {
     public static void main(String[] args) {
-        // bug1:除零
-        int a = 1 / 0;
+        // bug1:除零(新修复的缺陷)
+        int a = 2 / 1;
         System.out.println(a);
 
         // bug2:相同的两个if分支
@@ -21,41 +21,40 @@ public class mian {
         double dou = 1 + 2;
         System.out.println(dou);
 
-        // bug4:无用变量自我分配
-        a = a;
+        // bug4:无用变量自我分配（新删除的缺陷）
 
-        // bug5:无用变量自我分配
-        a = a;
+        // bug5:无用变量自我分配(新修改的缺陷)
+        dou = a;
 
-        // bug6:无用变量自我分配
-        a = a;
-
-        // bug7:无用变量自我分配
-        a = a;
-
-        // bug8:无用变量自我分配
-        a = a;
-
-        // bug9:无用变量自我分配
-        a = a;
-
-        // bug10:无用变量自我分配
-        a = a;
-
-        // bug11:无用变量自我分配
-        a = a;
-
-        // bug12:无用变量自我分配
-        a = a;
-
-        // bug13:无用变量自我分配
-        a = a;
-
-        // bug14:无用变量自我分配
-        a = a;
-
-        // bug15:无用变量自我分配
-        a = a;
+//        // bug6:无用变量自我分配
+//        a = a;
+//
+//        // bug7:无用变量自我分配
+//        a = a;
+//
+//        // bug8:无用变量自我分配
+//        a = a;
+//
+//        // bug9:无用变量自我分配
+//        a = a;
+//
+//        // bug10:无用变量自我分配
+//        a = a;
+//
+//        // bug11:无用变量自我分配
+//        a = a;
+//
+//        // bug12:无用变量自我分配
+//        a = a;
+//
+//        // bug13:无用变量自我分配
+//        a = a;
+//
+//        // bug14:无用变量自我分配
+//        a = a;
+//
+//        // bug15:无用变量自我分配
+//        a = a;
 
         // bug16:无用变量自我分配
         a = a;
@@ -71,5 +70,16 @@ public class mian {
 
         // bug20:无用变量自我分配
         a = a;
+
+        // bug21:新增的缺陷1
+        if(bool) {
+            System.out.println("666");
+        } else {
+            System.out.println("666");
+        }
+
+        // bug22:新增的缺陷2
+        dou = 1 + 1;
+        System.out.println(dou);
     }
 }
